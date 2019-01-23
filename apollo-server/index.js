713958656ec95c690445d7b6abf571801b5e0057
KuangPF <1633397286@qq.com>
2019-01-23 23:12:30 +0800

    feat: init apollo-server

---
+++ apollo-server/index.js
@@ -0,0 +1,34 @@
+const { ApolloServer, gql } = require('apollo-server');
+const books = [{
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+];
+const typeDefs = gql `
+  type Book {
+    title: String
+    author: String
+  }
+  type Query {
+    books: [Book]
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => books,
+    hello (root, args, context) {
+      return 'Hello world!'
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
