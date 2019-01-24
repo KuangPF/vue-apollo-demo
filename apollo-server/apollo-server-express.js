b3457d950605417d23e62ee3bb790ed0c7e8be8e
KuangPF <1633397286@qq.com>
2019-01-24 13:40:16 +0800

    feat: query

--- apollo-server/apollo-server-express.js
+++ apollo-server/apollo-server-express.js
@@ -1,44 +1,70 @@
-const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+const express = require('express')
+const { ApolloServer, gql } = require('apollo-server-express')
+const casual = require('casual')
 
-const books = [{
+const books = [
+  {
     title: 'Harry Potter and the Chamber of Secrets',
-    author: 'J.K. Rowling',
+    author: 'J.K. Rowling'
   },
   {
     title: 'Jurassic Park',
-    author: 'Michael Crichton',
-  },
-];
+    author: 'Michael Crichton'
+  }
+]
 
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+  type Tag {
+    id: Int
+    label: String
+  }
+  type Query {
+    books: [Book]
+    ping(message: String!): String
+    tags: [Tag]
+  }
+`
 
-const typeDefs = gql `
-type Book {
-  title: String
-  author: String
+// 生成一些标签
+var id = 0
+var tags = []
+for (let i = 0; i < 9; i++) {
+  addTag(casual.word)
 }
-type Query {
-  books: [Book]
-  ping(message: String!): String
-}
-`;
 
+function addTag(label) {
+  let t = {
+    id: id++,
+    label
+  }
+  tags.push(t)
+  return t
+}
 const resolvers = {
   Query: {
     books: () => books,
     ping(root, { message }, context) {
       return `Answering ${message}`
     },
-  },
-};
+    tags(root, args, context) {
+      return tags
+    }
+  }
+}
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers })
 
-const app = express();
-server.applyMiddleware({ app });
+const app = express()
+server.applyMiddleware({ app })
 
-const port = 4000;
+const port = 4000
 
 app.listen({ port }, () =>
-  console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`),
-);
+  console.log(
+    `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`
+  )
+)
--- src/components/HelloVueApollo.vue
+++ src/components/HelloVueApollo.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="vue-apollo">
     <div v-if="$apollo.loading">Loading...</div>
-    <p>Books:</p>
+    <h2>Books:</h2>
     <div v-for="(item, index) in books">
       <p>author:{{item.author}}</p>
       <p>title:{{item.title}}</p>
     </div>
     {{ping}}
+
+    <h3>轮询：</h3>
+    <div v-for="(item, index) in tags">
+      <p>id:{{item.id}}</p>
+      <p>label:{{item.label}}</p>
+    </div>
   </div>
 </template>
 
@@ -18,14 +24,23 @@ export default {
     ping: {
       // gql 查询
       query: gql`query PingMessage($message: String!) {
-      ping(message: $message)
-    }`,
+        ping(message: $message)
+      }`,
       // 静态参数
       variables: {
         message: 'Meow',
       },
     },
-  },
+    tags: {
+      query: gql`query tagList {
+      tags {
+        id,
+        label
+      }
+    }`,
+      pollInterval: 30000, // 毫秒
+    },
+  }
 }
 </script>
 
