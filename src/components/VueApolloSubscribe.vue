bc085ac35170c0936e5e30e78e353c0d251ad672
KuangPF <1633397286@qq.com>
2019-01-30 17:12:06 +0800

    feat: add query hello ping tags

--- src/components/VueApolloSubscribe.vue
+++ src/components/VueApolloSubscribe.vue
@@ -2,7 +2,12 @@
   <div class="subscribe-content">
     <h2>订阅：</h2>
     <h2>查询：</h2>
+    <h3>hello:</h3>
     {{hello}}
+    <h3>ping:</h3>
+    {{ping}}
+    <h3>tags:</h3>
+    {{tags}}
   </div>
 </template>
 
@@ -10,7 +15,27 @@
 import gql from 'graphql-tag'
 export default {
   apollo: {
-    hello:  gql`{hello}`
+    hello: gql`{hello}`,
+    ping: {
+      query: gql`query PingMessage($message: String!) {
+      ping(message: $message)
+    }`,
+      // 静态参数
+      variables: {
+        message: 'Meow',
+      },
+    },
+    tags: {
+      query: gql`query TagsType($type: String!) {
+              tags(type: $type) {
+                id label type
+              }
+            }`,
+      // 静态参数
+      variables: {
+        type: 'City',
+      },
+    }
   }
 }
 </script>
