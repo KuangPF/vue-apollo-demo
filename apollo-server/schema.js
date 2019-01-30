94618532b94e82e81bc0d805fd7b184bcc48e053
KuangPF <1633397286@qq.com>
2019-01-30 16:07:01 +0800

    feat: add subscribe server

--- apollo-server/schema.js
+++ apollo-server/schema.js
@@ -1,24 +1,85 @@
-'use strict'
+import { makeExecutableSchema } from 'graphql-tools';
 
-const { gql } = require('apollo-server-express')
+import Tags from './connectors';
+import { PubSub, withFilter } from 'graphql-subscriptions';
 
-const SomeSchema = require('./SomeSchema')
+const pubsub = new PubSub();
 
-const resolvers = require('../resolvers')
+const TAGS_CHANGED_TOPIC = 'tags_changed'
 
-const rootQuery = gql`
-  type Subscription {
-    subscriptionExample: SomeSchema
+const typeDefs = [`
+  type Tag {
+    id: Int
+    label: String
+    type: String
   }
-
+  type TagsPage {
+    tags: [Tag]
+    hasMore: Boolean
+  }
   type Query {
+    hello: String
+    ping(message: String!): String
+    tags(type: String!): [Tag]
+    tagsPage(page: Int!, size: Int!): TagsPage
+    randomTag: Tag
+    lastTag: Tag
   }
-
   type Mutation {
+    addTag(type: String!, label: String!): Tag
   }
-`
+  type Subscription {
+    tagAdded(type: String!): Tag
+  }
+  schema {
+    query: Query
+    mutation: Mutation
+    subscription: Subscription
+  }
+`];
 
-module.exports = {
-  typeDefs: [rootQuery, SomeSchema],
-  resolvers
-}
+const resolvers = {
+  Query: {
+    hello(root, args, context) {
+      return "Hello world!";
+    },
+    ping(root, { message }, context) {
+      return `Answering ${message}`;
+    },
+    tags(root, { type }, context) {
+      return Tags.getTags(type);
+    },
+    tagsPage(root, { page, size }, context) {
+      return Tags.getTagsPage(page, size);
+    },
+    randomTag(root, args, context) {
+      return Tags.getRandomTag();
+    },
+    lastTag(root, args, context) {
+      return Tags.getLastTag();
+    },
+  },
+  Mutation: {
+    addTag: async (root, { type, label }, context) => {
+      console.log(`adding ${type} tag '${label}'`);
+      const newTag = await Tags.addTag(type, label);
+      pubsub.publish(TAGS_CHANGED_TOPIC, { tagAdded: newTag });
+      return newTag;
+    },
+  },
+  Subscription: {
+    tagAdded: {
+      subscribe: withFilter(
+        () => pubsub.asyncIterator(TAGS_CHANGED_TOPIC),
+        (payload, variables) => payload.tagAdded.type === variables.type,
+      ),
+    }
+  },
+};
+
+const jsSchema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+export default jsSchema;
